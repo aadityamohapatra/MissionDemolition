cc471170a0eeb2a25dec4a08abc93c4507f5a5e6
Aaditya Mohapatra <aaditya.mohapatra@gmail.com>
2019-11-08 18:30:32 -0500

    Completed Mission Demolition
    completed mission demolition.cs and slingshot.cs

--- Assets/__Scripts/MissionDemolition.cs
+++ Assets/__Scripts/MissionDemolition.cs
@@ -7,55 +7,138 @@ using UnityEngine.UI;
 // TODO: You must set the values for the enum
 public enum GameMode
 {
-
+    idle,
+    playing,
+    levelEnd
 
 }
 
 
 // TODO: implement the MissionDemolition script
-public class MissionDemolition : MonoBehaviour {
+public class MissionDemolition : MonoBehaviour
+{
+    static public MissionDemolition S;
+    public GameObject[] castles;
+    public Text gtLevel;
+    public Text gtScore;
+    public Vector3 castlePos;
 
+    public bool _____________________________;
 
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Slingshot";
 
-
-
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-
+        S = this;
+        level = 0;
+        levelMax = castles.Length;
+        StartLevel();
     }
-	
-	void StartLevel()
+
+    void StartLevel()
     {
-
- 
+        if (castle != null)
+        {
+            Destroy(castle);
+        }
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach (GameObject pTemp in gos)
+        {
+            Destroy(pTemp);
+        }
+        castle = Instantiate(castles[level]) as GameObject;
+        castle.transform.position = castlePos;
+        shotsTaken = 0;
+        SwitchView("Both");
+        ProjectileLine.S.Clear();
+        Goal.goalMet = false;
+        ShowGT();
+        mode = GameMode.playing;
     }
 
-    void UpdateGUI()
+    void ShowGT()
     {
-
+        gtLevel.text = "Level: " + (level + 1) + " of " + levelMax;
+        gtScore.text = "Shots Taken: " + shotsTaken;
     }
+
+    void OnGUI()
+    {
+        Rect buttonRect = new Rect((Screen.width / 2) - 50, 10, 100, 24);
+        switch (showing)
+        {
+            case "Slingshot":
+                if (GUI.Button(buttonRect, "Show Castle"))
+                {
+                    SwitchView("Castle");
+                }
+                break;
+            case "Castle":
+                if (GUI.Button(buttonRect, "Show Both"))
+                {
+                    SwitchView("Both");
+                }
+                break;
+            case "Both":
+                if (GUI.Button(buttonRect, "Show Slingshot"))
+                {
+                    SwitchView("Slingshot");
+                }
+                break;
+        }
+    }
+
 
     void Update()
     {
+        ShowGT();
 
-
+        if (mode == GameMode.playing && Goal.goalMet)
+        {
+            mode = GameMode.levelEnd;
+            SwitchView("Both");
+            Invoke("NextLevel", 2f);
+        }
     }
 
     void NextLevel()
     {
-
+        level++;
+        if (level == levelMax)
+        {
+            level = 0;
+        }
+        StartLevel();
     }
 
     public void SwitchView(string eView = "")
     {
-
+        S.showing = eView;
+        switch (S.showing)
+        {
+            case "Slingshot":
+                FollowCam.POI = null;
+                break;
+            case "Castle":
+                FollowCam.POI = S.castle;
+                break;
+            case "Both":
+                FollowCam.POI = GameObject.Find("ViewBoth");
+                break;
+        }
     }
 
     // Static method that allows code anywhere to increment shotsTaken
     public static void ShotFired()
     {
-
+        S.shotsTaken++; 
     }
+
 }
+
